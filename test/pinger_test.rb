ad10f6f30445ae73fb4416c53c05649bf36618bd
Spencer Steffen <spencer@citrusme.com>
2011-12-29 00:11:08 -0800

    add test to ensure database error is raised when invalid pinged db url is specified

--- test/pinger_test.rb
+++ test/pinger_test.rb
@@ -2,10 +2,20 @@ require "test_helper"
 
 class PingerTest < MiniTest::Unit::TestCase
 
-  should "establish database connection" do        
+  should "establish database connection" do
     assert !Pinger.connection.nil?
   end
   
+  should "raise database error when invalid database url is provided" do
+    db_url = ENV["PINGER_DB"]
+    Pinger.instance_variable_set("@connection", nil)
+    ENV["PINGER_DB"] = "invalid://"
+    assert_raises Pinger::DatabaseError do
+      Pinger.connect
+    end
+    ENV["PINGER_DB"] = db_url
+  end
+
   should "alias connection to Pinger.db" do  
     assert_equal Pinger.connection, Pinger.db
   end
