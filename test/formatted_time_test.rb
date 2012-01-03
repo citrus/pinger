0f916f2383ab43c795a15cc46f18814f7ef88542
Spencer Steffen <spencer@citrusme.com>
2012-01-03 12:00:14 -0800

    ensure formatted time strips all leading zeros

--- test/formatted_time_test.rb
+++ test/formatted_time_test.rb
@@ -3,15 +3,20 @@ require "test_helper"
 class FormattedTimeTest < MiniTest::Unit::TestCase
 
   def setup
-    @time = FormattedTime.new(2011, 12, 26, 1, 1, 1)
+    @time = FormattedTime.new(2012, 1, 1, 1, 1, 1)
+    @time2 = FormattedTime.new(2010, 10, 10, 10, 10, 10)
   end
 
   should "respond to formatted method" do
     assert @time.respond_to?(:formatted)
   end
   
-  should "format time" do
-    assert_equal "12/26/2011 1:01:01am", @time.formatted
+  should "strip all leading zeros in formatted time" do
+    assert_equal "1/1/2012 1:01:01am", @time.formatted
+  end
+  
+  should "ensure only leading zeros are stripped" do
+    assert_equal "10/10/2010 10:10:10am", @time2.formatted
   end
   
 end
